18bf17b802655ca07bb00b44580e08bd44e1f72c
Veronika Mensikova <mensikov@arcig.cz>
2026-01-19 16:12:56 +0100

    Aktualizace sklad.py na v1.1 a přidání složky dokumentace

--- sklad.py
+++ sklad.py
@@ -1,6 +1,6 @@
 def vypis_sklad(seznam):
     for polozka in seznam:
-        print(f"Skladem: {polozka} (Verze 1.0)")
+        print(f"Skladem: {polozka} -Aktualizováno v1.1")
 
 if __name__ == "__main__":
     zbozi = ["Monitor", "Klávesnice", "Myš"]
